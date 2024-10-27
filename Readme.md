13813ee9986e4fb3ceedcb0537bdc2ff7ca1b5f3
Queng123 <quentin.brejoin@gmail.com>
2024-10-26 17:33:25 -0700

    add Readme

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Description
+
+This project was made during the AI/Journalism Hackathon in Berkeley, CA. The goal was to create a tool that helps news readers to understand if what they are reading as a lot of bias or not. This tool can also rewrite the text to make it less biased.
+
+# How to use
+
+## Backend
+
+Build the backend:
+```
+cd backend
+pip install -r requirements.txt
+flask run --host=0.0.0.0 --port=5000
+```
+
+Install and run ngrok:
+```
+ngrok http http://localhost:5000 
+```
+
+## Frontend
+
+Copy the add-on folder to the Chrome browser, on the extensions page.
+Remember to change the URL in the `popup.js` file to the one provided by ngrok.
+
--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,93 @@
-from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+from goose3 import Goose
+
+import openai
+import logging
+import os
+
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/check-page', methods=['POST'])
-def check_page():
-    return jsonify({'message': 'success'})
+logging.basicConfig(level=logging.INFO)
 
-
-@app.route('/rewrite', methods=['GET'])
+@app.route('/rewrite', methods=['POST'])
 def rewrite():
-    return jsonify({'message': 'success'})
+    url = request.json['url']
+    g = Goose()
 
+    article = g.extract(url=url).cleaned_text
+
+    preprompt = """
+    rewrite this article without any bias:
+    * DO NOT ADD ANY OTHER DATA
+    * THE NEW ARTICLE SHOULD BE NONE ORIENTED
+    * Ensure neutrality in tone by removing any language that implies judgment, preference, or criticism.
+    *Maintain factual accuracy, keeping all original details and figures without interpretation or exaggeration.
+    *Exclude language or phrasing that could indicate support, opposition, or affiliation with any viewpoint.
+    *Remove adjectives or adverbs that add emotional or persuasive weight to the content.
+    * Do not change the quotes or any citations
+    """
+
+    template = """
+    {preprompt}
+
+    Article:
+    {article}
+    """
+
+    prompt_template = PromptTemplate(input_variables=["preprompt", "article"], template=template)
+
+
+
+
+    prompt = prompt_template.format(preprompt=preprompt, article=article)
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    chain = LLMChain(llm=llm, prompt=prompt_template)
+    output = chain.run({"preprompt": prompt, "article": article})
+
+
+    return jsonify({'message': output})
+
+@app.route('/info', methods=['POST'])
+def infos():
+    url = request.json['url']
+    g = Goose()
+
+    article = g.extract(url=url).cleaned_text
+
+    preprompt = """
+        tell me if this website has an oriented article, and tell me why
+        also, if it's oriented, provide the orientation, from 0% to 100% (0% being the most left-leaning and 100% being the most right-leaning)
+        output as a json object, with the keys "reason", and "orientation"
+    """
+
+    template = """
+    {preprompt}
+
+    article:
+    {article}
+    """
+
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    
+    prompt_template = PromptTemplate(input_variables=["preprompt", "article"], template=template)
+    chain = LLMChain(llm=llm, prompt=prompt_template)
+    prompt = prompt_template.format(preprompt=preprompt, article=article)
+    output = chain.run({"preprompt": prompt, "article": article})
+    return jsonify({'message': output})
 
 if __name__ == '__main__':
     app.run(debug=True)
